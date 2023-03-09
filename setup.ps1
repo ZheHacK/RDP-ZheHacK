da1764db4dcc0ba8bfd82f453e15d8d7dc1402c6
ZheHacK <54292567+ZheHacK@users.noreply.github.com>
2023-03-09 22:03:15 +0700

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,4 @@
-<!--By ZheHacK-->
+# By ZheHacK
 
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
